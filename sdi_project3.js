d656f1c41e84e1193bc330aa1f992c26227e45b6
Scott Caruso <scottcaruso@fullsail.edu>
2012-04-17 00:41:06 -0400

    Adding some comments and moving some code around. Not a substantial edit.

--- sdi_project3.js
+++ sdi_project3.js
@@ -1,7 +1,7 @@
 //World variables
 
 var bugsPerHour = .2;
-var fixedPerHour = 1;
+var fixedPerHour = 1; //set this to a high value (i.e. 5) in order to test the else condition.
 
 //The below object details out information about the QA team.
 var qaTeam = { 
@@ -29,30 +29,27 @@ var devTeam = {
 	},
 };
 
-//The below determines how many more hours the Dev Team needs to work to fix all of the bugs.
-
+//The below determines if the Dev Team needs to work more hours to fix all of the bugs.
 var bugsFound = qaTeam.bugsFound(bugsPerHour);
 var bugsFixed = devTeam.bugsFixed(fixedPerHour);
-var bugDebt = bugsFound - bugsFixed
+var bugDebt = bugsFound - bugsFixed;
 
 if (bugsFound > bugsFixed){
 	console.log("Looks like we have to work more hours to fix all of the bugs.");
 	console.log("We are currently " + (Math.floor(bugDebt)) + " bugs behind.");
-} else {
-	console.log("We've beaten the QA team and fixed all of the bugs! Time for a pants party!")
-};
-
 //The below runs a while loop to determine how many more hours the team needs to work.
-var canFixDaily = devTeam.numberOfEngineers*8*fixedPerHour;
-var days = 0;
-console.log(canFixDaily);
-var i = bugDebt;
-while (i > 0) {
-	console.log("We fixed " + canFixDaily + " bugs yesterday, but we still have " + i + " more bugs to fix before we are done.");
-	i-=canFixDaily;
-	days++;
+		var canFixDaily = devTeam.numberOfEngineers*8*fixedPerHour;
+		var days = 0;
+		var i = bugDebt-canFixDaily;
+		while (i > 0) {
+			console.log("We fixed " + canFixDaily + " bugs yesterday, but we still have " + i + " more bugs to fix before we are done.");
+			i-=canFixDaily;
+			days++;
+		};
+			console.log("Finally! It only took us " + days + " days to get there, but we finally fixed all of the bugs!");
+	} else {
+	console.log("We've beaten the QA team without any additional work and fixed all of the bugs! Time for a pants party!")
 };
-console.log("Finally! It only took us " + days + " days to get there, but we finally fixed all of the bugs!");
 
 /*The below logs verify that the objects are properly outputting. Uncomment to verify.
 console.log( qaTeam );
