57c4f0fea680dad7cb75c5568c3ae28225c43159
Scott Caruso <scottcaruso@fullsail.edu>
2012-04-16 22:28:53 -0400

    Very basic object, to try to get my story started.

--- sdi_project3.js
+++ sdi_project3.js
@@ -1,1 +1,10 @@
-alert("JavaScript works!");
+var qaTeam = { 
+	methodology: "scrum",
+	manager: "Scott",
+	numberOfTesters: "12",	
+};
+
+console.log( qaTeam );
+console.log(qaTeam.methodology);
+console.log(qaTeam.manager);
+console.log(qaTeam.numberOfTesters);
