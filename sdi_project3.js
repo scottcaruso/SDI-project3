c60bd89b59cf6bbc0656a24b21cc4d3973b7240e
Scott Caruso <scottcaruso@fullsail.edu>
2012-04-16 23:18:36 -0400

    Added a dev team bugs fixed function.

--- sdi_project3.js
+++ sdi_project3.js
@@ -1,5 +1,4 @@
 var qaTeam = { 
-	methodology: "scrum",
 	manager: "Scott",
 	numberOfTesters: 12,
 	realhoursWorked: 4000,
@@ -10,8 +9,20 @@ var qaTeam = {
 	}
 };
 
+var devTeam = {
+	methodology: "scrum",
+	numberOfEngineers: 10,
+	realhoursWorked: 5000,
+	bugsFixed: function (fixedPerHour) {
+		var manhours = (devTeam.realhoursWorked * devTeam.numberOfEngineers);
+		var bugsFixed = (fixedPerHour * manhours);
+		return bugsFixed; 
+	}
+};
+
 console.log( qaTeam );
 console.log(qaTeam.methodology);
 console.log(qaTeam.manager);
 console.log(qaTeam.numberOfTesters);
 console.log(qaTeam.bugsFound(.2));
+console.log(devTeam.bugsFixed(.1));
