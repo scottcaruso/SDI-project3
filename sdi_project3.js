2ec55f9675f487fbbd19e28a4c04ff09ad4a681a
Scott Caruso <scottcaruso@fullsail.edu>
2012-04-16 22:59:47 -0400

    Added a function to my code that calculates the number of bugs found by the QA team.

--- sdi_project3.js
+++ sdi_project3.js
@@ -1,10 +1,17 @@
 var qaTeam = { 
 	methodology: "scrum",
 	manager: "Scott",
-	numberOfTesters: "12",	
+	numberOfTesters: 12,
+	realhoursWorked: 4000,
+	bugsFound: function (bugsPerHour) {
+		var manhours = (qaTeam.realhoursWorked * qaTeam.numberOfTesters);
+		var bugsFound = (bugsPerHour * manhours);
+		return bugsFound; 
+	}
 };
 
 console.log( qaTeam );
 console.log(qaTeam.methodology);
 console.log(qaTeam.manager);
 console.log(qaTeam.numberOfTesters);
+console.log(qaTeam.bugsFound(.2));
